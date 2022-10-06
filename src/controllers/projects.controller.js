4d7bb79e2aeb4b47b1b02d385865cf19596b6e51
hahs <alexandre_2420@outlook.com>
2022-10-06 12:17:43 -0500

    added update and delte  project functionality

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -11,6 +11,25 @@ export const getProjects = async (req, res) => {
   }
 };
 
+export const getProjectById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await Project.findByPk(id);
+
+    if (project === null) {
+      res.status(404).send({
+        status: 404,
+        message: `El project con el id: ${id} no se encuentra`,
+      });
+      return;
+    }
+
+    res.send(project);
+  } catch (error) {
+    res.status(500).send({ status: 500, message: error.message });
+  }
+};
+
 export const createProject = async (req, res) => {
   try {
     const { name, description, priority } = req.body;
@@ -27,6 +46,69 @@ export const createProject = async (req, res) => {
     });
 
     res.status(201).send(createdProject);
+  } catch (error) {
+    res.status(500).send({ status: 500, message: error.message });
+  }
+};
+
+export const deleteProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedProject = await Project.destroy({ where: { id } });
+
+    if (deletedProject === 0) {
+      res.status(404).send({
+        status: 404,
+        message: `El project con el id: ${id} no se encuentra`,
+      });
+      return;
+    }
+
+    res.send({ message: `El projecto con el id: ${id} fue destruido` });
+  } catch (error) {
+    res.status(500).send({ status: 500, message: error.message });
+  }
+};
+
+export const updateProject = async (req, res) => {
+  try {
+    const { body } = req;
+    const { id } = req.params;
+    const { name, description, priority } = body;
+
+    if (!name | !description | !priority) {
+      res.status(400).send("Back request!");
+      return;
+    }
+
+    const projectToUpdate = await Project.findByPk(id);
+
+    if (projectToUpdate === null) {
+      res.status(404).send({
+        status: 404,
+        message: `El project con el id: ${id} no se encuentra`,
+      });
+      return;
+    }
+
+    projectToUpdate.name = name;
+    projectToUpdate.priority = priority;
+    projectToUpdate.description = description;
+
+    projectToUpdate.save();
+
+    //otra forma de hacerlo
+    // await Project.update(
+    //   {
+    //     name,
+    //     description,
+    //     priority,
+    //   },
+    //   { where: { id: id } }
+    // );
+
+    res.send({ ...projectToUpdate.dataValues, name, priority, description });
   } catch (error) {
     res.status(500).send({ status: 500, message: error.message });
   }
--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -2,15 +2,18 @@ import { Router } from "express";
 //controllers
 import {
   getProjects,
+  getProjectById,
   createProject,
+  updateProject,
+  deleteProject,
 } from "../controllers/projects.controller.js";
 
 const router = Router();
 
 router.get("/projects", getProjects);
-router.get("/projects/:id");
+router.get("/projects/:id", getProjectById);
 router.post("/projects", createProject);
-router.put("/projects/:id");
-router.delete("/projects/:id");
+router.put("/projects/:id", updateProject);
+router.delete("/projects/:id", deleteProject);
 
 export default router;
